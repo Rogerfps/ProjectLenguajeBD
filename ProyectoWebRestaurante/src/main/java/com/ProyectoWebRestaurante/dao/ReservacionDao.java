f75faf27c8adbfce203d92f92bf7a47892c776c9
Sebastián Segura <ssegura30126@ufide.ac.cr>
2024-08-29 17:53:31 -0600

    deleteAll

--- ProyectoWebRestaurante/src/main/java/com/ProyectoWebRestaurante/dao/ReservacionDao.java
+++
@@ -1,9 +0,0 @@
-package com.ProyectoWebRestaurante.dao;
-
-import com.ProyectoWebRestaurante.domain.Reservacion;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReservacionDao
-        extends JpaRepository<Reservacion,Long> {
-    
-}
--- ProyectoWebRestaurante/src/main/java/com/ProyectoWebRestaurante/service/UsuarioDetailsService.java
+++
@@ -1,8 +0,0 @@
-package com.ProyectoWebRestaurante.service;
-
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-
-public interface UsuarioDetailsService {
-    UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
-}
